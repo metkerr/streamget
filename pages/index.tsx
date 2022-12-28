9fb5e0c18561a3b067c392aa9996ef30e81cbb10
Metker <64699075+metkerr@users.noreply.github.com>
2022-12-29 02:58:03 +0800

    update 10%

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Streamget</title>
+        <title>Streamget by Metker</title>
         <meta name="description" content="Spotify widget for streamer app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
---
+++ pages/login.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import spotifyLogo from "../public/logo.png";
+
+const Login = () => {
+  return (
+    <main className="flex min-h-screen min-w-screen">
+      <div id="login-wrapper" className="m-auto text-center">
+        <h1 className="text-[64px] font-semibold leading-[4.7rem]">
+          Streamget
+        </h1>
+        <p className="text-xl font-medium">
+          <span className="text-green-500">Spotify</span> widget for streamer
+          apps
+        </p>
+        <button className="select-none flex m-auto gap-1 items-center bg-green-500 py-1.5 px-5 rounded-sm mt-6 hover:bg-green-600 active:bg-green-700">
+          <Image src={spotifyLogo} alt="spotify logo / login" width={17} />
+          <span className="text-sm">Login</span>
+        </button>
+      </div>
+    </main>
+  );
+};
+export default Login;
